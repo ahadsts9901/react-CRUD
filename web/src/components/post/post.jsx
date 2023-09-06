ad80ecaac9bd2d55937255c7260680882dc2d102
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-09-06 19:26:50 +0500

    code

--- web/src/components/post/post.jsx
+++ web/src/components/post/post.jsx
@@ -1,20 +1,27 @@
 import "./post.css";
+import { TrashFill, PencilFill } from 'react-bootstrap-icons';
+import moment from 'moment'
 
 const Post = (props) => {
-    <div class=" post" id="">
-        <p class=" regards center" style="font-size: 0.7em;">3 minutes ago</p>
-        <h2 class=" scrollH">{props.title}</h2>
-        <p class=" scroll">{props.text}</p>
-        <div class=" space-around">
-            <p class=" regards">Regards! Muhammad Ahad</p><i class=" regards bi bi-pencil-fill">
-            </i><i class=" regards bi bi-trash-fill"></i>
+    const formattedTime = moment(props.time).fromNow();
+
+    return (
+        <div className="post" id="">
+            <p className="regards center" style={{ fontSize: '0.7em' }}>{formattedTime}</p>
+            <h2 className="scrollH">{props.title}</h2>
+            <p className="scroll">{props.text}</p>
+            <div className="space-around">
+                <p className="regards">Regards! Muhammad Ahad</p>
+                <TrashFill onClick={() => { props.del(props.postId) }} />
+                <PencilFill onClick={() => { props.edit(props.postId) }} />
+            </div>
         </div>
-        {console.log("Chal gaya")}
-    </div>
+    );
 };
 
+
 const NoPost = () => {
-    <h2 class="noPostsMessage">No post found...</h2>
+    return (<h2 className="noPostsMessage">No post found...</h2>)
 };
 
 export { Post, NoPost };
